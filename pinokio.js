62fca30d5617ab3b1feec1fa1fad1ec9fcacd751
CHTOZAKHAR <158284943+CHTOZAKHAR@users.noreply.github.com>
2024-04-02 16:21:10 +0300

    Add files via upload

---
+++ pinokio.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "DiffBIR",
+      description: "DiffBIR: Towards Blind Image Restoration with Generative Diffusion Prior",
+      icon: "icon.png",
+  menu: [{
+    html: "<i class='fa-solid fa-plug'></i> Install",
+    type: "link",
+    href: "install.json?run=true&fullscreen=true"
+  },
+  {
+    html: "<i class='fa-solid fa-power-off'></i> Start",
+    type: "link",
+    href: "start.json?run=true&fullscreen=true"
+  },
+  {
+    html: "<i class='fa-solid fa-gear'></i> Config",
+    type: "link",
+    href: "config.json?run=true&fullscreen=true"
+  }]
+}
